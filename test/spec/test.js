44890b70f1962e76c365413fc80579c4a8413b52
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2013-04-30 15:10:25 +0530

    Chai setup with mocha (should matchers)

--- test/spec/test.js
+++ test/spec/test.js
@@ -4,7 +4,7 @@
     describe('Give it some context', function () {
         describe('maybe a bit more context here', function () {
             it('should run here few assertions', function () {
-
+              [1,2,3].indexOf(4).should.equal(-1);
             });
         });
     });
